10ea0a21253851815a25bc78a7f2fbceab376258
Piotr.Mazurek <masourek@gmail.com>
2015-07-08 16:55:09 +0100

    Updating test requirements

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 from setuptools import setup, find_packages
+
+install_requires = [
+    "aiohttp>=0.15.3",
+    "asyncio==3.4.3",
+    "chardet==2.3.0",
+    "gevent==1.0.1",
+    "greenlet==0.4.5",
+    "pybreaker==0.2.3",
+    "PyYAML==3.11",
+    "redis==2.10.3",
+    "retrying==1.3.3",
+    "requests==2.7.0"
+]
+
+tests_require = [
+    "Contexts==0.10.2",
+    "fakeredis==0.6.1",
+    "freezegun==0.3.3",
+    "HTTPretty"
+]
+
 setup(
     name="AtomicPuppy",
     version="0.1.2",
     packages=find_packages(),
-    install_requires=[
-        "aiohttp>=0.15.3",
-        "asyncio==3.4.3",
-        "chardet==2.3.0",
-        "gevent==1.0.1",
-        "greenlet==0.4.5",
-        "pybreaker==0.2.3",
-        "PyYAML==3.11",
-        "redis==2.10.3",
-        "retrying==1.3.3",
-        "requests==2.7.0"
+    dependency_links=[
+        "git+https://github.com/OddBloke/HTTPretty.git@f899d1bda8234658c2cec5aab027cb5b7c42203c#egg=HTTPretty"
     ],
+    install_requires=install_requires,
+    tests_require=tests_require,
     url = 'https://github.com/madedotcom/atomicpuppy',
     download_url = 'https://github.com/madedotcom/atomicpuppy/tarball/0.1.2',
     description = 'A service-activator component for eventstore',
