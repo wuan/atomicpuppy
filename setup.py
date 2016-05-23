1491582a783b583da033ac85a7dabaf29611dd29
Francesco Pighi <francesco.pighi@gmail.com>
2016-05-23 16:34:58 +0200

    update the gevent version required

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = [
     "aiohttp>=0.15.3",
     "asyncio==3.4.3",
     "chardet==2.3.0",
-    "gevent==1.0.1",
+    "gevent>=1.1.1",
     "greenlet==0.4.5",
     "pybreaker==0.2.3",
     "PyYAML==3.11",
