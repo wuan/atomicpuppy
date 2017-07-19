1795b7138b217c63cf7322ecd8b116b98a48716a
Bob Gregory <bob@made.com>
2017-07-19 06:53:58 +0100

    Updating requirements for 3.6

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    "aiohttp==1.3.5",
-    "chardet==2.3.0",
+    "aiohttp>=1.3.5",
+    "chardet>=2.3.0",
     "gevent>=1.1.1",
-    "greenlet==0.4.12",
-    "pybreaker==0.2.3",
-    "PyYAML==3.12",
-    "redis==2.10.3",
-    "retrying==1.3.3",
-    "requests==2.7.0"
+    "greenlet>=0.4.12",
+    "pybreaker>=0.2.3",
+    "PyYAML>=3.12",
+    "retrying>=1.3.3",
+    "requests>=2.7.0"
 ]
 
 tests_require = [
@@ -32,6 +31,7 @@ setup(
     download_url='https://github.com/madedotcom/atomicpuppy/tarball/0.3.pre',
     description='A service-activator component for eventstore',
     author='Bob Gregory',
+    author_email='bob@made.com',
     keywords=['eventstore'],
     license='MIT',
 )
